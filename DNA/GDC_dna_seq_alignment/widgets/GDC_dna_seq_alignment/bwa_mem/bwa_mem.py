ed21b898baab6b9a7689fdbed344ece7350ace51
Hong Hung <lhhunghimself@gmail.com>
2020-12-24 13:11:24 -0800

    added new temp DNA workflows

---
+++ DNA/GDC_dna_seq_alignment/widgets/GDC_dna_seq_alignment/bwa_mem/bwa_mem.py
@@ -0,0 +1,64 @@
+import os
+import glob
+import sys
+import functools
+import jsonpickle
+from collections import OrderedDict
+from Orange.widgets import widget, gui, settings
+import Orange.data
+from Orange.data.io import FileFormat
+from DockerClient import DockerClient
+from BwBase import OWBwBWidget, ConnectionDict, BwbGuiElements, getIconName, getJsonName
+from PyQt5 import QtWidgets, QtGui
+
+class OWbwa_mem(OWBwBWidget):
+    name = "bwa_mem"
+    description = "Aligns paired-end sequences"
+    priority = 20
+    icon = getIconName(__file__,"bwa-mem.png")
+    want_main_area = False
+    docker_image_name = "biodepot/bwa-samtools"
+    docker_image_tag = "gdcalign__alpine_3.12"
+    inputs = [("readgroup",str,"handleInputsreadgroup"),("reference",str,"handleInputsreference"),("trigger",str,"handleInputstrigger")]
+    outputs = [("outputFile",str)]
+    pset=functools.partial(settings.Setting,schema_only=True)
+    runMode=pset(0)
+    exportGraphics=pset(False)
+    runTriggers=pset([])
+    triggerReady=pset({})
+    inputConnectionsStore=pset({})
+    optionsChecked=pset({})
+    outputFile=pset(None)
+    readgroup=pset(None)
+    reference=pset(None)
+    fastq_files=pset([])
+    threads=pset(None)
+    minscore=pset(None)
+    def __init__(self):
+        super().__init__(self.docker_image_name, self.docker_image_tag)
+        with open(getJsonName(__file__,"bwa_mem")) as f:
+            self.data=jsonpickle.decode(f.read())
+            f.close()
+        self.initVolumes()
+        self.inputConnections = ConnectionDict(self.inputConnectionsStore)
+        self.drawGUI()
+    def handleInputsreadgroup(self, value, *args):
+        if args and len(args) > 0: 
+            self.handleInputs("readgroup", value, args[0][0], test=args[0][3])
+        else:
+            self.handleInputs("inputFile", value, None, False)
+    def handleInputsreference(self, value, *args):
+        if args and len(args) > 0: 
+            self.handleInputs("reference", value, args[0][0], test=args[0][3])
+        else:
+            self.handleInputs("inputFile", value, None, False)
+    def handleInputstrigger(self, value, *args):
+        if args and len(args) > 0: 
+            self.handleInputs("trigger", value, args[0][0], test=args[0][3])
+        else:
+            self.handleInputs("inputFile", value, None, False)
+    def handleOutputs(self):
+        outputValue=None
+        if hasattr(self,"outputFile"):
+            outputValue=getattr(self,"outputFile")
+        self.send("outputFile", outputValue)
