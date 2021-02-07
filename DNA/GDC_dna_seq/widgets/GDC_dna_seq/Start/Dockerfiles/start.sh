5f177b1d463cb3dc204246c52d357ea15f010fb8
Bob Schmitz III <14095796+rgschmitz1@users.noreply.github.com>
2021-02-07 02:22:03 +0000

    Updated start.sh for Variant Effect Predictor
    Signed-off-by: Bob Schmitz III <14095796+rgschmitz1@users.noreply.github.com>

--- DNA/GDC_dna_seq/widgets/GDC_dna_seq/Start/Dockerfiles/start.sh
+++ DNA/GDC_dna_seq/widgets/GDC_dna_seq/Start/Dockerfiles/start.sh
@@ -4,3 +4,8 @@ printenv
 
 mkdir -p $work_dir || exit 1
 mkdir -p $genome_dir || exit 1
+# Variant Effect Predictor (DNA seq workflow) needs permissions set for UID 999
+if [ -n "$vep_dir" ]; then
+	mkdir -p $vep_dir || exit 1
+	chown 999 $vep_dir
+fi
