4857d1f93dd1e867108afaab46a45ddc501636a2
lhhung <lhhunghimself@gmail.com>
2021-03-24 01:10:43 -0700

    Added annotation options to haplotype caller

--- DNA/GATK_germline_variant/widgets/GATK_germline_variant/gatk_HC/Dockerfiles/runHC.sh
+++ DNA/GATK_germline_variant/widgets/GATK_germline_variant/gatk_HC/Dockerfiles/runHC.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-echo "gatk --java-options \"-Xmx4g\" HaplotypeCaller $@"
-gatk --java-options "-Xmx4g" HaplotypeCaller $@
+putFlagInArray(){
+ flags+=($(echo $1 | sed "s/[[\,]/$2/g" | sed "s/[]\"]//g"))
+}
+flags=($@)
+
+#gatk put flags multiple times for multiple values instead of putting multiple values after one flag
+#bwb passes these flags as arrays in json format which need to be parsed
+[ -n $annotation ] && putFlagInArray $annotation ' -G '
+
+echo "gatk --java-options \"-Xmx4g\" HaplotypeCaller ${flags[@]}"
+gatk --java-options "-Xmx4g" HaplotypeCaller ${flags[@]}
