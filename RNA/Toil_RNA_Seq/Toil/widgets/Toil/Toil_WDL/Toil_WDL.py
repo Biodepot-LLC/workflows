71836267edda0feae5bccc4f901f8b8b9f9fbf58
varikmp <varikmp@uw.edu>
2021-03-26 11:56:59 -0700

    added more workflows for AWS demo

---
+++ RNA/Toil_RNA_Seq/Toil/widgets/Toil/Toil_WDL/Toil_WDL.py
@@ -0,0 +1,55 @@
+import os
+import glob
+import sys
+import functools
+import jsonpickle
+from collections import OrderedDict
+from Orange.widgets import widget, gui, settings
+import Orange.data
+from Orange.data.io import FileFormat
+from DockerClient import DockerClient
+from BwBase import OWBwBWidget, ConnectionDict, BwbGuiElements, getIconName, getJsonName
+from PyQt5 import QtWidgets, QtGui
+
+class OWToil_WDL(OWBwBWidget):
+    name = "Toil_WDL"
+    description = "Toil WDL"
+    priority = 1
+    icon = getIconName(__file__,"wdl.png")
+    want_main_area = False
+    docker_image_name = "varikmp/toil"
+    docker_image_tag = "5.2.0"
+    inputs = [("inputFile",str,"handleInputsinputFile"),("Trigger",str,"handleInputsTrigger")]
+    outputs = [("OutputDir",str)]
+    pset=functools.partial(settings.Setting,schema_only=True)
+    runMode=pset(0)
+    exportGraphics=pset(False)
+    runTriggers=pset([])
+    triggerReady=pset({})
+    inputConnectionsStore=pset({})
+    optionsChecked=pset({})
+    WDL_FILE=pset(None)
+    JSON_FILE=pset(None)
+    def __init__(self):
+        super().__init__(self.docker_image_name, self.docker_image_tag)
+        with open(getJsonName(__file__,"Toil_WDL")) as f:
+            self.data=jsonpickle.decode(f.read())
+            f.close()
+        self.initVolumes()
+        self.inputConnections = ConnectionDict(self.inputConnectionsStore)
+        self.drawGUI()
+    def handleInputsinputFile(self, value, *args):
+        if args and len(args) > 0: 
+            self.handleInputs("inputFile", value, args[0][0], test=args[0][3])
+        else:
+            self.handleInputs("inputFile", value, None, False)
+    def handleInputsTrigger(self, value, *args):
+        if args and len(args) > 0: 
+            self.handleInputs("Trigger", value, args[0][0], test=args[0][3])
+        else:
+            self.handleInputs("inputFile", value, None, False)
+    def handleOutputs(self):
+        outputValue=None
+        if hasattr(self,"OutputDir"):
+            outputValue=getattr(self,"OutputDir")
+        self.send("OutputDir", outputValue)
