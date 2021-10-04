b11b1c6f0a011ee61c542d0e3486530488d8bcf9
varikmp <varikmp@uw.edu>
2021-10-04 01:30:20 -0700

    added an example of workflow using ContainerProfiler

---
+++ general/ContainerProfiler/widgets/ContainerProfiler/image_viewer/image_viewer.py
@@ -0,0 +1,49 @@
+import os
+import glob
+import sys
+import functools
+import jsonpickle
+from collections import OrderedDict
+from Orange.widgets import widget, gui, settings
+import Orange.data
+from Orange.data.io import FileFormat
+from DockerClient import DockerClient
+from BwBase import OWBwBWidget, ConnectionDict, BwbGuiElements, getIconName, getJsonName
+from PyQt5 import QtWidgets, QtGui
+
+class OWimage_viewer(OWBwBWidget):
+    name = "image_viewer"
+    description = "Image Viewer"
+    priority = 10
+    icon = getIconName(__file__,"eog.png")
+    want_main_area = False
+    docker_image_name = "varikmp/eog"
+    docker_image_tag = "latest"
+    inputs = [("File",str,"handleInputsFile")]
+    outputs = [("File",str)]
+    pset=functools.partial(settings.Setting,schema_only=True)
+    runMode=pset(0)
+    exportGraphics=pset(False)
+    runTriggers=pset([])
+    triggerReady=pset({})
+    inputConnectionsStore=pset({})
+    optionsChecked=pset({})
+    FILES=pset(None)
+    def __init__(self):
+        super().__init__(self.docker_image_name, self.docker_image_tag)
+        with open(getJsonName(__file__,"image_viewer")) as f:
+            self.data=jsonpickle.decode(f.read())
+            f.close()
+        self.initVolumes()
+        self.inputConnections = ConnectionDict(self.inputConnectionsStore)
+        self.drawGUI()
+    def handleInputsFile(self, value, *args):
+        if args and len(args) > 0: 
+            self.handleInputs("File", value, args[0][0], test=args[0][3])
+        else:
+            self.handleInputs("inputFile", value, None, False)
+    def handleOutputs(self):
+        outputValue=None
+        if hasattr(self,"File"):
+            outputValue=getattr(self,"File")
+        self.send("File", outputValue)
