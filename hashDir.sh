02a79f50854c853c16e1383f3f639a2e27da3f24
lhhung <lhhunghimself@gmail.com>
2023-09-13 20:30:37 -0700

    test3

---
+++ hashDir.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+dir=$1
+tarcmd=tar
+if [ "$(uname)" == "Darwin" ]; then
+    tarcmd=gtar
+fi
+if [ -z "$dir" ]; then
+    echo "Usage: $0 <dir>"
+    exit 1
+fi
+cmd="$tarcmd -C $dir -cf - --sort=name --mtime='UTC 2019-01-01' --group=0 --owner=0 --numeric-owner . | sha256sum"
+
+echo $cmd
+eval $cmd
