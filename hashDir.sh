36cd78a3bcfc9b15b3150c5d87ae35d234052d92
lhhung <lhhunghimself@gmail.com>
2023-09-13 23:19:33 -0700

    remove -T flag and added mode flag to tar command

--- hashDir.sh
+++ hashDir.sh
@@ -8,7 +8,7 @@ if [ -z "$dir" ]; then
     echo "Usage: $0 <dir>"
     exit 1
 fi
-cmd="$tarcmd -C $dir -cf - --sort=name --portability  --mtime='UTC 2019-01-01' --group=0 --owner=0 --numeric-owner -T . | sha256sum"
+cmd="$tarcmd -C $dir -cf - --mode='600' --sort=name --portability  --mtime='UTC 2019-01-01' --group=0 --owner=0 --numeric-owner . | sha256sum"
 
 #echo $cmd
 eval $cmd | awk '{print $1}'
